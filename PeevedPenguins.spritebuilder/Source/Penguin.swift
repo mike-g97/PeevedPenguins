9d91873169d0b029a7d41130ae9257c14c123bed
Mikhael E Gonzalez <mikhael.gonzalez@gmail.com>
2015-06-29 16:22:15 -0400

    Finished adding custom objects using SpriteBuilder.

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.swift
@@ -0,0 +1,14 @@
+//
+//  Penguin.swift
+//  PeevedPenguins
+//
+//  Created by Mikhael Gonzalez on 6/28/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Penguin: CCSprite {
+    
+
+}
---
+++ PeevedPenguins.spritebuilder/Source/Seal.swift
@@ -0,0 +1,14 @@
+//
+//  Seal.swift
+//  PeevedPenguins
+//
+//  Created by Mikhael Gonzalez on 6/28/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Seal: CCSprite {
+   
+    
+}
