cf7c01765330a1319ef1f6c7630ba8a211836397
Mikhael E Gonzalez <mikhael.gonzalez@gmail.com>
2015-06-30 11:22:27 -0400

    Finished creating levels.

--- PeevedPenguins.spritebuilder/Source/Gameplay.swift
+++ PeevedPenguins.spritebuilder/Source/Gameplay.swift
@@ -11,10 +11,14 @@ import UIKit
 class Gameplay: CCNode {
     weak var gamePhysicsNode: CCPhysicsNode!
     weak var catapultArm: CCNode!
+    weak var levelNode: CCNode!
+    weak var contentNode: CCNode!
     
     // called when CCB file has completed loading
     func didLoadFromCCB() {
         userInteractionEnabled = true
+        let level = CCBReader.load("Levels/Level1")
+        levelNode.addChild(level)
     }
     
     // called on every touch in this scene
@@ -28,20 +32,26 @@ class Gameplay: CCNode {
         let penguin = CCBReader.load("Penguin") as! Penguin
         
         // position the penguin at the bowl of the catapult
-        println(catapultArm.position)
-        
         penguin.position = ccpAdd(catapultArm.position, CGPoint(x: 16, y: 50))
         
         // add the penguin to the gamePhysicsNode (because the penguin has physics enabled)
         gamePhysicsNode.addChild(penguin)
-        
-        
         
         // manually create & apply a force to launch the penguin
         let launchDirection = CGPoint(x: 1, y: 0)
         let force = ccpMult(launchDirection, 8000)
         penguin.physicsBody.applyForce(force)
         
+        // ensure followed object is in visible are when starting
+        position = CGPoint.zeroPoint
+        let actionFollow = CCActionFollow(target: penguin, worldBoundary: boundingBox())
+        contentNode.runAction(actionFollow)
+        
+    }
+    
+    func retry() {
+        let gameplayScene = CCBReader.loadAsScene("Gameplay")
+        CCDirector.sharedDirector().presentScene(gameplayScene)
     }
     
 }
