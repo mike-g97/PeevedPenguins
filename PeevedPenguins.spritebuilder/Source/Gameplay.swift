e6bf434140d026649fced8ee292825679c77c7eb
Mikhael E Gonzalez <mikhael.gonzalez@gmail.com>
2015-06-30 10:24:10 -0400

    Finished completing the gameplay scene.

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.swift
@@ -0,0 +1,47 @@
+//
+//  Gameplay.swift
+//  PeevedPenguins
+//
+//  Created by Mikhael Gonzalez on 6/29/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Gameplay: CCNode {
+    weak var gamePhysicsNode: CCPhysicsNode!
+    weak var catapultArm: CCNode!
+    
+    // called when CCB file has completed loading
+    func didLoadFromCCB() {
+        userInteractionEnabled = true
+    }
+    
+    // called on every touch in this scene
+    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        launchPenguin()
+    }
+    
+    func launchPenguin() {
+        
+        // loads the Penguin.ccb we have set up in SpriteBuilder
+        let penguin = CCBReader.load("Penguin") as! Penguin
+        
+        // position the penguin at the bowl of the catapult
+        println(catapultArm.position)
+        
+        penguin.position = ccpAdd(catapultArm.position, CGPoint(x: 16, y: 50))
+        
+        // add the penguin to the gamePhysicsNode (because the penguin has physics enabled)
+        gamePhysicsNode.addChild(penguin)
+        
+        
+        
+        // manually create & apply a force to launch the penguin
+        let launchDirection = CGPoint(x: 1, y: 0)
+        let force = ccpMult(launchDirection, 8000)
+        penguin.physicsBody.applyForce(force)
+        
+    }
+    
+}
